67515e2adc91b277f3127d98671bc4cdaaa9e25a
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-27 15:07:57 +0000

    server updated

--- dist/server.js
+++ dist/server.js
@@ -5,13 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-//For env File 
+const indexRouter_1 = __importDefault(require("./routes/indexRouter"));
+//For env File
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8000;
-app.get('/', (req, res) => {
-    res.send('Welcome to Express & TypeScript Server');
-});
+app.use(indexRouter_1.default);
 app.listen(port, () => {
     console.log(`Server is Fire at http://localhost:${port}`);
 });
--- server.ts
+++ server.ts
@@ -1,15 +1,14 @@
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response, Application } from "express";
+import dotenv from "dotenv";
+import indexRouter from "./routes/indexRouter";
 
-//For env File 
+//For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
-});
+app.use(indexRouter);
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
