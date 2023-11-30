53604dda79b3e38771744b43e3fd4c38ad4f51d8
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-30 15:16:04 +0000

    auth middleware created, chk login & admin status

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/UserModel";
+import TokenBlackList from "../models/tokenBlackList";
+
+export const isLoggedIn = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    res.status(401).json({ error: "Access denied. No token provided." });
+    return;
+  }
+
+  try {
+    const findToken = await TokenBlackList.findOne({ token });
+
+    if (findToken) {
+      res.status(401).json({ error: "Access denied. Token has been blacklisted." });
+      return;
+    }
+
+    const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      res.status(401).json({ error: "Invalid token. User not found!" });
+      return;
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid token." });
+  }
+};
+
+export const isAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  if (!req.user?.isAdmin) {
+    res.status(403).json({ error: "ACCESS DENIED! Admin permission needed." });
+    return;
+  }
+
+  next();
+};
