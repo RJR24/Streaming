aabfe12bc3466632a71c0752f5d7bf66c2449716
Kaveh <rezarezaeijamiuk@gmail.com>
2023-11-30 15:09:57 +0000

    folder changed

--- routes/categoryRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var categoryController_1 = require("../controllers/categoryController");
-var categoryRouter = (0, express_1.Router)();
-categoryRouter.get("/categories", categoryController_1.getAllCategories);
-exports.default = categoryRouter;
--- routes/userRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getAllUsers } from "../controllers/userController";
-const userRouter = Router();
-
-
-userRouter.get("/users", getAllUsers)
-
-
-export default userRouter
