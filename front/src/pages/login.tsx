b2c8ce43a4f77b0bbbe10403a98bc51e1b0cf0e9
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-08 11:13:59 +0000

    login and signup form page added

---
+++ front/src/pages/login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../components/login/Login";
+import SignInSignUpHeader from "../components/sIn-sUp-header/SignInSignUpHeader";
+
+const LoginForm = () => {
+  return (
+    <>
+     
+      <Login />
+    </>
+  );
+};
+
+export default LoginForm;
