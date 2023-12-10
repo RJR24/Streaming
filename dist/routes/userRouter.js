6daafa99f58727e931867cbe0d2f11cfe0c1238a
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-10 10:24:58 +0000

    routes changed to /auth/signup"

--- dist/routes/userRouter.js
+++ dist/routes/userRouter.js
@@ -6,8 +6,8 @@ const auth_1 = require("../middlewares/auth");
 const userRouter = (0, express_1.Router)();
 userRouter.get("/profile", auth_1.isLoggedIn, userController_1.getUserProfile);
 userRouter.put("/update", auth_1.isLoggedIn, userController_1.updateUserProfile);
-userRouter.post("/signup", userController_1.registerUser);
-userRouter.post("/login", userController_1.loginUser);
-userRouter.post("/logout", userController_1.logoutUser);
+userRouter.post("/auth/signup", userController_1.registerUser);
+userRouter.post("/auth/login", userController_1.loginUser);
+userRouter.post("/auth/logout", userController_1.logoutUser);
 exports.default = userRouter;
 //# sourceMappingURL=userRouter.js.map
