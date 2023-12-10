9c66079febccd46f1228b7d1d6daa74ea3fdf906
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-10 10:52:36 +0000

    header styles improved

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,21 +1,19 @@
+import "./header-styles.css";
 
-// components/header/Header.jsx
-import "./header-styles.css"
-
-import Image from 'next/image';
-import NetflixLogo from '../../assets/image-logo/Netflix-logo.svg';
-import search from '../../assets/image-logo/search.svg';
-import giftBox from '../../assets/image-logo/giftBox.svg';
-import bell from '../../assets/image-logo/bell.svg';
-import arrowDown from '../../assets/image-logo/arrowDown.svg';
-import profileImg from '../../assets/image-logo/ProfileIMG.svg';
+import Image from "next/image";
+import NetflixLogo from "../../assets/image-logo/Netflix-logo.svg";
+import search from "../../assets/image-logo/search.svg";
+import giftBox from "../../assets/image-logo/giftBox.svg";
+import bell from "../../assets/image-logo/bell.svg";
+import arrowDown from "../../assets/image-logo/arrowDown.svg";
+import profileImg from "../../assets/image-logo/ProfileIMG.svg";
 
 const Header = () => {
   return (
-    <div className="header flex justify-between px-14 py-6 text-slate-100">
+    <div className="header flex justify-between px-14 py-6 text-slate-100 max-h-md">
       <div className="flex items-center gap-12 ">
         <div className=" w-32 h-16 flex align-middle">
-          <Image src={NetflixLogo} alt="Netflix Logo"  />
+          <Image src={NetflixLogo} alt="Netflix Logo" />
         </div>
         <div className="header-left flex gap-5">
           <div>Home</div>
@@ -25,13 +23,13 @@ const Header = () => {
           <div>My List</div>
         </div>
       </div>
-      <div className="flex items-center gap-5">
+      <div className="header-right flex items-center gap-5">
         <div>
-          <Image src={search} alt="search" width={20}  />
+          <Image src={search} alt="search" width={20} />
         </div>
         <div>Kids</div>
         <div>
-          <Image src={giftBox} alt="gift Box" width={20}  />
+          <Image src={giftBox} alt="gift Box" width={20} />
         </div>
         <div>
           <Image src={bell} alt="bell" width={20} />
