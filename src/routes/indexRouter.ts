e66ffb3c7d6f91c9e0dede9b959fa4c70a5f7884
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-11 13:36:58 +0000

    index router updated after adding movie detail page

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { index } from "../controllers/indexController";
 import userRouter from "./userRouter";
 import categoryRouter from "./categoryRouter";
+import MovieRouter from "../routes/movieRoutes";
 const indexRouter = Router();
 
-indexRouter.use(userRouter)
-indexRouter.use(categoryRouter)
+indexRouter.use(userRouter);
+indexRouter.use(categoryRouter);
+indexRouter.use(MovieRouter);
 
+indexRouter.get("/", index);
 
-indexRouter.get("/", index)
-
-
-export default indexRouter
+export default indexRouter;
