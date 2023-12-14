ad88cbd89d802105784bee4990fe2ce0a9cf3eac
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-14 23:59:47 +0000

    Movie Details route added!

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,8 +4,10 @@ import {
   loginUser,
   registerUser,
   logoutUser,
-  updateUserProfile
+  updateUserProfile,
+  getMyListMovieDetails
 } from "../controllers/userController";
+import { addToMyList, removeFromMyList } from "../controllers/userController";
 import { isLoggedIn } from "../middlewares/auth";
 
 const userRouter = Router();
@@ -15,5 +17,15 @@ userRouter.put("/update", isLoggedIn, updateUserProfile);
 userRouter.post("/auth/signup", registerUser);
 userRouter.post("/auth/login", loginUser);
 userRouter.post("/auth/logout", logoutUser);
+
+
+// Add to My List
+userRouter.post("/api/user/myList/add/:movieId", isLoggedIn, addToMyList);
+// Remove from My List
+userRouter.post("/api/user/myList/remove/:movieId", isLoggedIn, removeFromMyList);
+// Get My List Movie Details
+userRouter.get("/api/user/myList/:movieId", isLoggedIn, getMyListMovieDetails);
+
+
 
 export default userRouter;
