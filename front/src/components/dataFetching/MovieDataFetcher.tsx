fdae88e7b6b1c382af940b94b120af58c2fb765c
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-17 22:18:10 +0000

    improved the reusability

--- front/src/components/dataFetching/MovieDataFetcher.tsx
+++ front/src/components/dataFetching/MovieDataFetcher.tsx
@@ -1,71 +1,32 @@
+// MovieDataFetcher.tsx
 import React, { useEffect, useState } from "react";
 import Carousel from "../carousels/Carousel";
 import axios from "axios";
 
-const MovieDataFetcher: React.FC = () => {
-  const [trendingMovies, setTrendingMovies] = useState([]);
-  const [top10MoviesUK, setTop10MoviesUK] = useState([]);
+interface MovieDataFetcherProps {
+  endpoint: string;
+  transformFunction: (movie: any) => { title: string; imageUrl: string; id: string };
+  title: string;
+}
+
+const MovieDataFetcher: React.FC<MovieDataFetcherProps> = ({ endpoint, transformFunction, title }) => {
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    // Function to fetch trending movies
-    const fetchTrendingMovies = async () => {
+    const fetchData = async () => {
       try {
-        const response = await axios.get(
-          "https://api.themoviedb.org/3/movie/popular?api_key=7b269e05a4ae4f5629b1515cafb76014",
-          {
-            params: {
-              api_key: process.env.STREAMING_MOVIE_API_KEY,
-            },
-          }
-        );
-        const trendingMoviesData = response.data.results.map((movie) => ({
-          title: movie.title,
-          imageUrl: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-          id: movie.id,
-        }));
-        console.log(response.data);
-
-        setTrendingMovies(trendingMoviesData);
-        console.log(trendingMovies);
+        const response = await axios.get(endpoint);
+        const moviesData = response.data.results.map(transformFunction);
+        setMovies(moviesData);
       } catch (error) {
-        console.error("Error fetching trending movies:", error);
+        console.error(`Error fetching data from ${endpoint}:`, error);
       }
     };
 
-    // Function to fetch top 10 movies in the UK
-    const fetchTop10MoviesUK = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.themoviedb.org/3/movie/top_rated?api_key=7b269e05a4ae4f5629b1515cafb76014",
-          {
-            params: {
-              api_key: process.env.STREAMING_MOVIE_API_KEY,
-              region: "0044", // Country code for the UK
-            },
-          }
-        );
-        const Top10MoviesUK = response.data.results.map((movie) => ({
-          title: movie.title,
-          imageUrl: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
-          id: movie.id,
-        }));
-        setTop10MoviesUK(Top10MoviesUK);
-      } catch (error) {
-        console.error("Error fetching top 10 movies in the UK:", error);
-      }
-    };
+    fetchData();
+  }, [endpoint, transformFunction]);
 
-    // Call the functions when the component mounts
-    fetchTrendingMovies();
-    fetchTop10MoviesUK();
-  }, []); // Empty dependency array means this effect runs once when the component mounts
-
-  return (
-    <>
-      <Carousel title="Trending" items={trendingMovies} />
-      <Carousel title="Today's Top 10 List in UK" items={top10MoviesUK} />
-    </>
-  );
+  return <Carousel title={title} items={movies} />;
 };
 
 export default MovieDataFetcher;
