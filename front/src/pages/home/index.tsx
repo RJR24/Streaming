eaef54e4841bfc01c1e8b103e3dca40007cad6a8
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-18 10:15:56 +0000

    index updated

--- front/src/pages/home/index.tsx
+++ front/src/pages/home/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 
 import Header from "../../components/header/Header";
 import HeroSection from "../../components/hero/HeroSection";
 import Carousel from "../../components/carousels/Carousel";
 import MovieDataFetcher from "../../components/dataFetching/MovieDataFetcher";
+import UserMyList from "../../components/userMyList/UserMyList";
+import TrendingMovies from "../../components/movieCategoriesCarousels/TrendingMovies";
+import Top10MoviesUK from "../../components/movieCategoriesCarousels/TopTenUk";
+import Upcoming from "../../components/movieCategoriesCarousels/UpcomingMovies";
+import PopularMovies from "../../components/movieCategoriesCarousels/TrendingMovies";
+import TvShows from "../../components/movieCategoriesCarousels/TvShows";
 
 const Home = () => {
   // data for the carousel items
@@ -18,21 +24,9 @@ const Home = () => {
     imageUrl: `/movies-cover-images/Kaveh-keep-watching/MoviePoster-${index}.png`,
   }));
 
-  const trendingMovies = Array.from({ length: 10 }, (_, index) => ({
-    title: `Trending Movie ${index}`,
-    imageUrl: `/movies-cover-images/trending/MoviePoster-${index}.png`,
-    width: 285,
-    height: 160,
-  }));
-
   const netflixOriginals = Array.from({ length: 10 }, (_, index) => ({
     title: `Netflix Original ${index}`,
     imageUrl: `/movies-cover-images/Netflix-original-content/MoviePoster-${index}.png`,
-  }));
-
-  const TodaysTop10ListInUK = Array.from({ length: 10 }, (_, index) => ({
-    title: `Todays Top 10 List in UK ${index}`,
-    imageUrl: `/movies-cover-images/MoviePoster-${index}.png`,
   }));
 
   const watchAgain = Array.from({ length: 10 }, (_, index) => ({
@@ -40,10 +34,7 @@ const Home = () => {
     imageUrl: `/movies-cover-images/watch-Again/MoviePoster-${index}.png`,
   }));
 
-  const MyList = Array.from({ length: 10 }, (_, index) => ({
-    title: `My List ${index}`,
-    imageUrl: `/movies-cover-images/MoviePoster-${index}.png`,
-  }));
+  useEffect(() => {});
 
   return (
     <>
@@ -53,18 +44,14 @@ const Home = () => {
 
         {/* Carousel for Movies */}
 
-        <Carousel title="Popular On Netflix" items={PopularOnNetflix} />
+        <PopularMovies />
         <Carousel title="Kaveh, keep watching" items={KavehKeepWatching} />
-        {/* <Carousel title="Trending" items={trendingMovies} /> */}
+        <TvShows />
         <Carousel title="Netflix Original Content" items={netflixOriginals} />
-        {/* <Carousel
-          title="Today's Top 10 List in UK"
-          items={TodaysTop10ListInUK}
-        /> */}
-        <Carousel title="Watch Again" items={watchAgain} />
 
-        <MovieDataFetcher />
-        <Carousel title="My List" items={MyList} />
+        <UserMyList />
+        <Upcoming />
+        <Top10MoviesUK />
       </div>
     </>
   );
