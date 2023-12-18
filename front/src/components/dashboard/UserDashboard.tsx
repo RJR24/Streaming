8198fc75c0ad7e7b7e8e5853800de4b2b9b6cc39
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-18 22:27:07 +0000

    working on user dashboard

--- front/src/components/dashboard/UserDashboard.tsx
+++ front/src/components/dashboard/UserDashboard.tsx
@@ -86,26 +86,12 @@ const UserDashboard = () => {
                 >
                   <div className="relative flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
                     <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        className="w-8 h-8 group-hover:text-indigo-400"
-                      >
-                        <circle
-                          cx="12"
-                          cy="5.5"
-                          r="4.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        />
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M12 10c2 0 3.5 2.5 3.5 5v2.5H8V15c0-2.5 1.5-5 3.5-5z"
-                        />
-                      </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 group-hover:text-indigo-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+                              </svg>
+
+
+
                     </div>
                     <div>
                       <p className="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">
@@ -378,7 +364,7 @@ const UserDashboard = () => {
                     <div className="flex flex-row items-center">
                       <div className="text-3xl p-4">📺</div>
                       <div className="p-2">
-                        <p className="text-xl font-bold">Tv Show</p>
+                        <p className="text-xl font-bold">Tv Show on Netflix</p>
                         <p className="text-gray-500 font-medium">Maia Kipper</p>
                         <p className="text-gray-500 text-sm">23 Nov 2022</p>
                       </div>
@@ -513,6 +499,38 @@ const UserDashboard = () => {
                         <p className="text-xl font-bold">Trending movies</p>
                         <p className="text-gray-500 font-medium">Tony Ankel</p>
                         <p className="text-gray-500 text-sm">22 Nov 2022</p>
+                      </div>
+                    </div>
+                    <div className="border-t border-white/5 p-4">
+                      <a
+                        href="#"
+                        className="inline-flex space-x-2 items-center text-center"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                          />
+                        </svg>
+                        <span>Info</span>
+                      </a>
+                    </div>
+                  </div>
+                  <div className="bg-black/60 to-white/5 rounded-lg">
+                    <div className="flex flex-row items-center">
+                      <div className="text-3xl p-4">➕</div>
+                      <div className="p-2">
+                        <p className="text-xl font-bold">Add a new category</p>
+                        <p className="text-gray-500 font-medium">Jonny Nite</p>
+                        <p className="text-gray-500 text-sm">23 Nov 2022</p>
                       </div>
                     </div>
                     <div className="border-t border-white/5 p-4">
