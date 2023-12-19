acf6d340d2817c426876ccbf2763b8f188dc4cff
Kaveh <rezarezaeijamiuk@gmail.com>
2023-12-19 18:02:55 +0000

    new page for user dashboard created

---
+++ front/src/pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserDashboard from "../../components/dashboard/UserDashboard";
+
+const Dashboard = () => {
+  return <UserDashboard />;
+};
+
+export default Dashboard;
