5e7e28100754fcafda5dafcd19e17665d5de99be
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-03 09:49:28 +0000

    working on categories

---
+++ front/src/pages/categories/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PopularMoviesCat from '../../components/categories/PopularMovies'
+
+const categories = () => {
+  return (
+    <div>
+      <PopularMoviesCat />
+    </div>
+  )
+}
+
+export default categories
