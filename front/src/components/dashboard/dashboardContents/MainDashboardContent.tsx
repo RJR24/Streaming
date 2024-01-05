d5898812817e30bcc92f10f7c1cf2bcebc17742b
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-05 15:24:56 +0000

    text changed

--- front/src/components/dashboard/dashboardContents/MainDashboardContent.tsx
+++ front/src/components/dashboard/dashboardContents/MainDashboardContent.tsx
@@ -487,7 +487,7 @@ const DashboardContent = () => {
                     d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
                   />
                 </svg>
-                <span>Click here to add</span>
+                <span>Click to add</span>
               </a>
             </div>
              {/* Render the CategoryModal */}
@@ -527,7 +527,7 @@ const DashboardContent = () => {
                     d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
                   />
                 </svg>
-                <span>Click here to add</span>
+                <span>Click to remove</span>
               </a>
             </div>
              {/* Render the CategoryModal */}
