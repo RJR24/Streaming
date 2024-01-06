dc2dba068b019bdcc81b3fc27c43b4a39327751b
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-06 21:27:27 +0000

    name changed

--- front/src/pages/adminDashboard/index.tsx
+++ front/src/pages/adminDashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import UserDashboard from "../../components/dashboard/AdminDashboard";
+import AdminDashboard from "../../components/dashboard/AdminDashboard";
 
 const Dashboard = () => {
-  return <UserDashboard />;
+  return <AdminDashboard />;
 };
 
 export default Dashboard;
