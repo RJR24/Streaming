fa17c7f820f3c186e6e9f7ac430ca08c5051f510
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-06 14:02:54 +0000

    name changed

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -1,6 +1,10 @@
-import "./header-styles.css";
-
+// Navbar.tsx
+import React, { useState } from "react";
 import Image from "next/image";
+import UserDropdownModal from "./UserDropdownModal";
+
+import "./navbar-styles.css";
+
 import NetflixLogo from "../../assets/image-logo/Netflix-logo.svg";
 import search from "../../assets/image-logo/search.svg";
 import giftBox from "../../assets/image-logo/giftBox.svg";
@@ -8,14 +12,16 @@ import bell from "../../assets/image-logo/bell.svg";
 import arrowDown from "../../assets/image-logo/arrowDown.svg";
 import profileImg from "../../assets/image-logo/ProfileIMG.svg";
 
-const Header = () => {
+const Navbar: React.FC = () => {
+  const [isUserDropdownVisible, setIsUserDropdownVisible] = useState(false);
+
   return (
-    <div className="header flex justify-between px-14 py-6 text-slate-100 max-h-md">
+    <div className="navbar flex justify-between px-14 py-6 text-slate-100 max-h-md">
       <div className="flex items-center gap-12 ">
-        <div className=" w-32 h-16 flex align-middle">
+        <div className="w-32 h-16 flex align-middle">
           <Image src={NetflixLogo} alt="Netflix Logo" />
         </div>
-        <div className="header-left flex gap-5">
+        <div className="navbar-left flex gap-5">
           <div>Home</div>
           <div>TV Shows</div>
           <div>Movies</div>
@@ -23,7 +29,11 @@ const Header = () => {
           <div>My List</div>
         </div>
       </div>
-      <div className="header-right flex items-center gap-5">
+      <div
+        className="navbar-right relative flex items-center gap-5"
+        onMouseEnter={() => setIsUserDropdownVisible(true)}
+        onMouseLeave={() => setIsUserDropdownVisible(false)}
+      >
         <div>
           <Image src={search} alt="search" width={20} />
         </div>
@@ -34,13 +44,16 @@ const Header = () => {
         <div>
           <Image src={bell} alt="bell" width={20} />
         </div>
-        <div className="flex items-center gap-2">
+        <div className="user-avatar flex items-center gap-2">
           <Image src={profileImg} alt="" width={32} />
           <Image src={arrowDown} alt="" width={20} height={20} />
+          <div className="UserDropdownModal">
+            <UserDropdownModal isVisible={isUserDropdownVisible} />
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Header;
+export default Navbar;
