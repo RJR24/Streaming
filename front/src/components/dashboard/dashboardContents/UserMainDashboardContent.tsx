86e5481eb4fec45c5681c5a55dfb3a29d5849ff3
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-08 17:10:49 +0000

    syntax error fixed

--- front/src/components/dashboard/dashboardContents/UserMainDashboardContent.tsx
+++ front/src/components/dashboard/dashboardContents/UserMainDashboardContent.tsx
@@ -8,23 +8,22 @@ const UserMainDashboardContent = () => {
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
   const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
 
-  
   const handleOpenAddModal = () => {
     setIsAddModalOpen(true);
   };
-  
+
   const handleCloseAddModal = () => {
     setIsAddModalOpen(false);
   };
-  
+
   const handleOpenRemoveModal = () => {
     setIsRemoveModalOpen(true);
   };
-  
+
   const handleCloseRemoveModal = () => {
     setIsRemoveModalOpen(false);
   };
-  
+
   const apiKey = process.env.NEXT_PUBLIC_API_KEY;
   const tmdbUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -43,13 +42,13 @@ const UserMainDashboardContent = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="w-10 h-10 text-white"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                   />
                 </svg>
@@ -65,13 +64,13 @@ const UserMainDashboardContent = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                       />
                     </svg>
@@ -87,7 +86,7 @@ const UserMainDashboardContent = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="w-10 h-10 text-white"
                 >
@@ -95,8 +94,8 @@ const UserMainDashboardContent = () => {
                     cx="12"
                     cy="12"
                     r="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <line x1="12" y1="12" x2="12" y2="7" />
                   <line x1="12" y1="12" x2="16" y2="12" />
@@ -113,13 +112,13 @@ const UserMainDashboardContent = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                       />
                     </svg>
@@ -135,13 +134,13 @@ const UserMainDashboardContent = () => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="w-10 h-10 text-white"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
                   />
                 </svg>
@@ -157,13 +156,13 @@ const UserMainDashboardContent = () => {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                       />
                     </svg>
@@ -180,7 +179,7 @@ const UserMainDashboardContent = () => {
           id="categoriesStats"
           className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
         >
-          <CategoryDataFetcher 
+          <CategoryDataFetcher
             categoryUrl={`${tmdbUrl}/movie/popular?api_key=${apiKey}`}
             transformFunction={(movie) => ({
               title: movie.title,
@@ -208,13 +207,13 @@ const UserMainDashboardContent = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
                         className="w-6 h-6"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
                         />
                       </svg>
@@ -254,13 +253,13 @@ const UserMainDashboardContent = () => {
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
                         className="w-6 h-6"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
                         />
                       </svg>
@@ -271,8 +270,6 @@ const UserMainDashboardContent = () => {
               </div>
             )}
           </CategoryDataFetcher>
-
-
         </div>
       </div>
     </div>
@@ -281,10 +278,8 @@ const UserMainDashboardContent = () => {
 
 export default UserMainDashboardContent;
 
-
-
-
-{/* <div className="new-category bg-black/60 hover:bg-white/10 to-white/5 rounded-lg">
+{
+  /* <div className="new-category bg-black/60 hover:bg-white/10 to-white/5 rounded-lg">
 <div className="flex flex-row items-center">
   <div className="text-3xl p-4">➕</div>
   <div className="p-2">
@@ -305,13 +300,13 @@ export default UserMainDashboardContent;
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="w-6 h-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
       />
     </svg>
@@ -322,9 +317,11 @@ export default UserMainDashboardContent;
 {isAddModalOpen && (
 <AddCategoryModal onClose={handleCloseAddModal} />
 )}
-</div> */}
+</div> */
+}
 
-{/* <div className="Remove-category bg-black/60 hover:bg-white/10 to-white/5 rounded-lg">
+{
+  /* <div className="Remove-category bg-black/60 hover:bg-white/10 to-white/5 rounded-lg">
 <div className="flex flex-row items-center">
   <div className="text-3xl p-4">➖</div>
   <div className="p-2">
@@ -345,13 +342,13 @@ export default UserMainDashboardContent;
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="w-6 h-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
       />
     </svg>
@@ -363,4 +360,5 @@ export default UserMainDashboardContent;
 {isRemoveModalOpen && (
 <RemoveCategoryModal onClose={handleCloseRemoveModal} />
 )}
-</div> */}
+</div> */
+}
