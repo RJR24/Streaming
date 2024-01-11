c17a090a4708aa372e963fd030e722af828e709b
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-11 18:52:01 +0000

    navbar and footer added

--- front/src/pages/dashboard/index.tsx
+++ front/src/pages/dashboard/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import UserDashboard from "../../components/dashboard/UserDashboard";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 const Dashboard = () => {
-  return <UserDashboard />;
+  return (
+    <div>
+      <Navbar />
+      <UserDashboard />
+      <Footer />
+    </div>
+  );
 };
 
 export default Dashboard;
