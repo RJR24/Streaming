348a13f3d376c7b562733dedc8eb3a59844d9261
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-11 18:51:32 +0000

    footer added

--- front/src/pages/adminDashboard/index.tsx
+++ front/src/pages/adminDashboard/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import AdminDashboard from "../../components/dashboard/AdminDashboard";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
 
 const Dashboard = () => {
-  return <AdminDashboard />;
+  return (
+    <div>
+      <Navbar />
+      <AdminDashboard />
+      <Footer />
+    </div>
+  );
 };
 
 export default Dashboard;
