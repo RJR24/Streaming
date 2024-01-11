11d40839430f9c0294c295a4914f0bd7ae2eacfa
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-11 18:28:04 +0000

    working on hero section

--- front/src/components/hero/HeroSection.tsx
+++ front/src/components/hero/HeroSection.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-
-import "./hero-styles.css";
 import Image from "next/image";
 
+import "./hero-styles.css";
 import play from "../../assets/image-logo/Play.svg";
 import info from "../../assets/image-logo/info.svg";
 import Top10 from "../../assets/image-logo/Top10.svg";
@@ -10,33 +9,23 @@ import Top10 from "../../assets/image-logo/Top10.svg";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      <div className="hero-movie-details flex flex-col gap-4 relative top-[20rem] left-[2.75rem]">
-        <div className="movie-title font-bold text-6xl">Money Heist</div>
+      <div className="hero-movie-details flex flex-col gap-8 relative top-[20rem] left-[2.75rem] max-w-[480px]">
+        <div className="movie-title font-bold text-4xl md:text-lg sm:text-sm ">Money Heist</div>
         <div className="text-3xl flex align-middle gap-4">
           <Image src={Top10} alt="Top10 sign" width={32} height={32} />
           Number 4 in UK Today
         </div>
-        <div
-          className="text-white
-text-[26px]
-font-normal
-font-['Netflix
-Sans']"
-        >
+        <div className="text-white text-sm  font-normal font-['Netflix Sans']">
           Eight thieves take hostages and lock themselves in the Royal Mint of
           Spain as a criminal mastermind manipulates the police to carry out his
           plan. Tokyo inspired a cast full of city names.
         </div>
-        <div className="play-info flex align-middle gap-4">
-          <div
-            className="flex bg-white cursor-pointer rounded bg-opacity-90 hover:bg-opacity-100 justify-center items-center gap-5 px-[30px] py-4 text-neutral-900 font-medium
-font-['Netflix
-Sans'] text-[22px]"
-          >
+        <div className="play-info flex items-center gap-4 mt-4">
+          <div className="flex bg-white cursor-pointer rounded bg-opacity-90 hover:bg-opacity-100 justify-center items-center gap-2 md:gap-4 px-2 md:px-6 py-2 md:py-4 text-neutral-900 font-medium font-['Netflix Sans'] text-sm md:text-lg">
             <Image src={play} alt="play sign" width={32} />
             <button>Play</button>
           </div>
-          <div className=" flex cursor-pointer bg-white bg-opacity-30 hover:bg-opacity-40 rounded justify-center items-center gap-5 px-[30px] py-4 text-[22px] font-medium font-['Netflix Sans']">
+          <div className="flex cursor-pointer bg-white bg-opacity-30 hover:bg-opacity-40 rounded justify-center items-center gap-2 md:gap-4 px-2 md:px-6 py-2 md:py-4 text-sm md:text-lg font-medium font-['Netflix Sans']">
             <Image src={info} alt="info sign" width={32} />
             <button>more information</button>
           </div>
