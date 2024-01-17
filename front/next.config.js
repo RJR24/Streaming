4fb0cf1e0cb0ed000a0fb8444905a4987950a378
Kaveh <rezarezaeijamiuk@gmail.com>
2024-01-17 00:42:07 +0000

    deploy

--- front/next.config.js
+++ front/next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org", 'images.generated.photos'],
   },
-  
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
